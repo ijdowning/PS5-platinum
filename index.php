b6afa5e1bd63445f05abbbf760afc5d72a773f94
Luke Downing <lukeraymonddowning@gmail.com>
2022-09-30 16:11:44 +0100

    Adds array_filter.

--- index.php
+++ index.php
@@ -5,50 +5,57 @@
     <title>PS5_Games_To_platinum</title>
 </head>
 <body>
-    <h1>
-        <?php
-        echo 'Welcome To Your List Of PS5 Games'
-        ?>
-    </h1>
-    <h2>
-        <?php
-        echo 'Enjoy Completing These Amazing Games!'
-        ?>
-    </h2>
-    <p>
-    <?php $gameslibrary = json_decode(file_get_contents('games.json'), true);?>
-        <table>
-            <thead>
-                <th>
-                    Game Name
-                </th>
-                <th>
-                    Completion Status
-                </th>
-            </thead>
-            <tbody>
-                <?php
-                foreach($gameslibrary as $game){?>
-                <tr>
-                    <td>
-                        <?= $game["name"]; ?>
-                    </td>
-                    <td>
-                        <?= $game["completion_percentage"]; ?>
-                    </td>
-                </tr>
-                <?php } ?>   
+<h1>
+    <?php
+    echo 'Welcome To Your List Of PS5 Games'
+    ?>
+</h1>
+<h2>
+    <?php
+    echo 'Enjoy Completing These Amazing Games!'
+    ?>
+</h2>
+<p>
+    <?php
+    $gameslibrary = json_decode(file_get_contents('games.json'), true);
 
-            </tbody>
-        </table>
-    </p>
-    <h3>
-        <?php
-        echo 'Completed Games:'
+    $completedGames = array_filter($gameslibrary, function ($game) {
+        return true;
+    });
+    ?>
+<table>
+    <thead>
+    <th>
+        Game Name
+    </th>
+    <th>
+        Completion Status
+    </th>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($completedGames as $game) {
         ?>
-    </h3> 
-    <p>
-        
-    </p> 
+        <tr>
+            <td>
+                <?= $game["name"]; ?>
+            </td>
+            <td>
+                <?= $game["completion_percentage"]; ?>
+            </td>
+        </tr>
+    <?php } ?>
+
+    </tbody>
+</table>
+</p>
+<h3>
+    <?php
+    echo 'Completed Games:'
+    ?>
+</h3>
+<p>
+
+</p>
 </body>
 </html>
