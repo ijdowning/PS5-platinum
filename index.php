25601ccd0da9fe3427bfed94bd03e28158a9b258
ijdowning <isaacdowning2000@gmail.com>
2022-09-30 15:22:14 +0100

    intial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PS5_Games_To_platinum</title>
+</head>
+<body>
+    <h1>
+        <?php
+        echo 'Welcome To Your List Of PS5 Games'
+        ?>
+    </h1>
+    <h2>
+        <?php
+        echo 'Enjoy Completing These Amazing Games!'
+        ?>
+    </h2>
+    <p>
+        <table>
+            <tr>
+                <?php
+                $gameslibrary['Gran Tousismo 7','Horizon: Forbidden West','Gotham Knights'];
+                ?>
+            </tr>
+        </table>
+    </p>
+    <h3>
+        <?php
+        echo 'Completed Games:'
+        ?>
+    </h3>
+    
+</body>
+</html>
